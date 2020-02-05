ce85934a1465164e71f534550ce27c29b88dc673
frzDazzT <58939437+frzDazzT@users.noreply.github.com>
2020-02-05 12:11:33 +0100

    project
    :+1:

---
+++ Assets/SharedScripts/Pickup.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour
+{
+    GameObject player;
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.GetComponent<Key>())
+        {
+            if (collision.gameObject.GetComponent<Key>().normalKey)
+            {
+                //add key to key inventory
+                Debug.Log("key added");
+                //destroy key
+            }
+
+            if (collision.gameObject.GetComponent<Key>().fakeKey)
+            {
+                //take off 20 seconds or something
+                Debug.Log("time taken off");
+                //destroy key
+            }
+        }
+    }
+}
---
+++ Assets/SharedScripts/PlayerMove.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    Rigidbody rb;
+    public float moveSpeed = 4;
+    public float jumpForce = 3;
+    Vector3 moveDir;
+    bool canJump = true;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        //movement
+        // rb.MovePosition(transform.position + (transform.forward * Input.GetAxis("Vertical") * moveSpeed * Time.fixedDeltaTime) +
+        //     (transform.right * Input.GetAxis("Horizontal") * moveSpeed * Time.fixedDeltaTime));
+
+        moveDir.x = Input.GetAxis("Horizontal");
+        moveDir.z = Input.GetAxis("Vertical");
+
+        transform.Translate(moveDir * moveSpeed * Time.fixedDeltaTime);
+
+
+        //jump
+        if (Input.GetButtonDown("Jump") && canJump)
+        {
+            canJump = false;
+            rb.velocity = new Vector3(0, jumpForce, 0);
+            rb.AddForce(transform.forward * 50f);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            canJump = true;
+        }
+    }
+}
