647997b917f32258cfc3dd58b175ee687a3e0c08
frzDazzT <58939437+frzDazzT@users.noreply.github.com>
2020-02-26 10:35:02 +0100

    MainMenuCode
    Main menu code

---
+++ ontwikkeling/Assets/SharedScripts/MenuManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+    GameObject player;
+    GameObject start;
+    public void PauseButtons(int index)
+    {
+        switch (index)
+        {
+            case 0:
+                player.GetComponent<Player>().UnPause();
+                break;
+            case 1:
+                player.transform.position = start.transform.position;
+                player.GetComponent<Player>().UnPause();
+                //resart timer
+                break;
+            case 2:
+                //button settings
+                break;
+            case 3:
+                Application.Quit();
+                break;
+        }
+
+
+    }
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        start = GameObject.FindGameObjectWithTag("Start");
+    }
+}
